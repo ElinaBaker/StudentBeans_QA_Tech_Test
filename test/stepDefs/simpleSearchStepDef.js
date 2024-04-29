99edfe2a99fee831b2cfd003e5bb6ed4f648a330
ElinaBaker <165950960+ElinaBaker@users.noreply.github.com>
2024-04-29 20:52:22 +0100

    Update simpleSearchStepDef.js

--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -7,3 +7,15 @@ Given('I am on the studentbeans homepage', async () => {
   await simpleSearchPageObject.goToHomePage()
   await simpleSearchPageObject.verifyHomePage()
 })
+
+When('I open the search bar', async  () => {
+    await simpleSearchPageObject.openSearchBar()
+})
+
+When('I enter "samsung"', async  (searchWord) => {
+    await simpleSearchPageObject.enterSearchTerm(searchWord)
+})
+
+Then('I should select the {int}th {string} search listing', async  (listingIndex, searchWord) => {
+    await simpleSearchPageObject.selectSearchListing(listingIndex, searchWord)
+})
