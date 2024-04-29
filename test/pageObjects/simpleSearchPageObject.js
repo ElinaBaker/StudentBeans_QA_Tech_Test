3764f83cbe32582c7091285492df6de117b9202f
ElinaBaker <165950960+ElinaBaker@users.noreply.github.com>
2024-04-29 20:50:52 +0100

    Update simpleSearchPageObject.js

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -9,6 +9,43 @@ class SimpleSearchPageObject extends ParentPageObject {
   async verifyHomePage () {
     await this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+
+  async openSearchBar() {
+    const searchBarButton = await $('[class="!w-full !py-3 xl:!p-0 css-tgua54"]')
+     await searchBarButton.click();
+  }
+
+  async enterSearchWord(searchWord) {
+    const searchInputField = await $('[class="!w-full !py-3 xl:!p-0 css-tgua54"]');
+      await searchInputField.setValue(samsung);
+
+  }
+
+   async selectSearchListing(listingIndex, searchWord) {
+      const searchListings = await $$('[class="flex items-start rounded-1 p-3 hover:bg-grey-50"]');
+        let samsungCount = 0;
+        let fourthSamsungListing;
+
+        for (const listing of searchListings) {
+            const listingText = await listing.getText();
+            if (listingText.includes('samsung')) {
+                samsungCount++;
+                if (samsungCount === 4) {
+                    fourthSamsungListing = listing;
+                    break;
+                }
+            }
+        }
+
+        if (fourthSamsungListing) {
+            await fourthSamsungListing.click();
+        } else {
+            throw new Error('The 4th "Samsung" search listing was not found.');
+      }
+
+   }
+
 }
+
 
 module.exports = SimpleSearchPageObject
